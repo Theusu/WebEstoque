9cbf037ace8cfb482c6c8de7613b31ecb9650bbe
Theusu <poligonos11@yahoo.com>
2019-02-20 20:54:12 -0300

    Alterado para instalar o samba

--- git_config.sh
+++ git_config.sh
@@ -2,3 +2,13 @@
 
 git config --global user.name "Theusu"
 git config --global user.email "poligonos11@yahoo.com"
+
+
+sudo apt-get -y install samba samba-common-bin
+sudo bash -c 'cat samba.txt >> /etc/samba/smb.conf'
+sudo smbpasswd -s -a fatec<<EOF
+fatec
+fatec
+EOF
+sudo /etc/init.d/smbd restart
+sudo /etc/init.d/nmbd restart
