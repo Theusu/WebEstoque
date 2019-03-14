6c3ff5e6c31fa89dd99ad220c3f62829b296818a
Theusu <poligonos11@yahoo.com>
2019-03-13 21:10:51 -0300

    Aula 13.03.2019

--- database/migrations/2019_02_21_001342_create_classifications_table.php
+++ database/migrations/2019_02_21_001342_create_classifications_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateClassificationTable extends Migration
+class CreateClassificationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,9 @@ class CreateClassificationTable extends Migration
      */
     public function up()
     {
-        Schema::create('classification', function (Blueprint $table) {
+        Schema::create('classifications', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('descricao', 30);
             $table->timestamps();
         });
     }
@@ -26,6 +27,6 @@ class CreateClassificationTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('classification');
+        Schema::dropIfExists('classifications');
     }
 }
