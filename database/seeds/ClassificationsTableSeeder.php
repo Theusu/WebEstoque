4dcb16a310f26909e55e1aa9001a4130240b256f
Theusu <matheuscalves1995@gmail..com>
2019-03-20 21:57:13 -0300

    Alterações do dia 20/03

---
+++ database/seeds/ClassificationsTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ClassificationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+
+        for ($i=1; $i<=20; $i++) {
+            DB::table ('classifications')->insert([
+                'descricao' => $faker->text(30),
+                'created_at' => \Carbon\Carbon::now()
+            ]);  
+        }        
+    }
+}
