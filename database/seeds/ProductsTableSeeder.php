b05e7aa621a918e15607136a17ee814553eace37
Theusu <poligonos11@yahoo.com>
2019-03-27 21:54:32 -0300

    Fim da aula 27/03/2019

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -14,12 +14,26 @@ class ProductsTableSeeder extends Seeder
         for ($i=1; $i<=20; $i++) { 
             
             $faker = Faker\Factory::create();
+
+            //array das classifications ID
+            $classificationsID = DB::table('classifications')
+                ->select('id')
+                ->get();
+
+            //array das providers ID
+            $providersID = DB::table('providers')
+                ->select('id')
+                ->get();
             
             DB::table ('products')->insert([
                 'descricao' => $faker->text(30),
                 'qtd' => $faker->randomNumber(),
                 'prc_venda' => $faker->randomFloat(2, 100, 200),
-                'prc_compra' => $faker->randomFloat(2, 50, 100),
+                'prc_compra' => $faker->randomFloat(2, 100, 200),
+                
+                'providers_id' => $faker->randomElement($providersID)->id, // randomização dos ids
+                'classifications_id' => $faker->randomElement($classificationsID)->id, // randomização dos ids
+
                 'created_at' => \Carbon\Carbon::now()
             ]);  
         }
