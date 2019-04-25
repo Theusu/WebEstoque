6deeb46d357d77c7170f5f1e125f40a24fa5ac12
Theusu <poligonos11@yahoo.com>
2019-04-24 21:42:31 -0300

    Aula 24/04

--- routes/web.php
+++ routes/web.php
@@ -25,14 +25,14 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/classifications','ClassificationController@index')->name('classifications.index');
 
-Route::get('/classifications/create','ClassificationsController@create')->name('classifications.create');
+Route::get('/classifications/create','ClassificationController@create')->name('classifications.create');
 
-Route::post('/classifications','ClassificationsController@store')->name('classifications.store');
+Route::post('/classifications','ClassificationController@store')->name('classifications.store');
 
-Route::get('/classifications/{id}','ClassificationsController@show')->name('classifications.show');
+Route::get('/classifications/{id}','ClassificationController@show')->name('classifications.show');
 
-Route::get('/classifications/{id}/edit','ClassificationsController@edit')->name('classifications.edit');
+Route::get('/classifications/{id}/edit','ClassificationController@edit')->name('classifications.edit');
 
-Route::put('/classifications/{id}','ClassificationsController@update')->name('classifications.update');
+Route::put('/classifications/{id}','ClassificationController@update')->name('classifications.update');
 
-Route::delete('/classifications/{id}/delete','ClassificationsController@destroy')->name('classifications.destroy');
+Route::delete('/classifications/{id}/delete','ClassificationController@destroy')->name('classifications.destroy');
